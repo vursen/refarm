000433bbe9b44756ece48b18fee42b6008de9d39
Sergey Vinogradov <pettsonchello@gmail.com>
2020-08-28 23:25:23 +0300

    Improve the cart store

--- stores/cart.ts
+++ stores/cart.ts
@@ -1,17 +1,24 @@
-import { State, Action } from '../core'
+import { State, Action, Getter } from '../core'
 
 export class CartStore {
   @State productIds: number[] = []
 
+  constructor ({ productIds }) {
+    this.productIds = productIds
+  }
+
+  @Getter
   has (productId: number) {
     return this.productIds.find((id) => productId === id)
   }
 
-  @Action add (productId: number) {
+  @Action
+  add (productId: number) {
     this.productIds.push(productId)
   }
 
-  @Action remove (productId: number) {
+  @Action
+  remove (productId: number) {
     this.productIds = this.productIds.filter((id) => productId !== id)
   }
 }
